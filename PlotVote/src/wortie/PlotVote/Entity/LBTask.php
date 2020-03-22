5428587ccc80ae47438e39d6d7ac0d38ae635a72
Atech <mc8atech@gmail.com>
2020-03-22 14:20:11 +0000

    Initial Upload
    Another Plugin

---
+++ PlotVote/src/wortie/PlotVote/Entity/LBTask.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace wortie\PlotVote\Entity;
+
+use wortie\PlotVote\Entity\LbEntity;
+use pocketmine\scheduler\Task;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+use pocketmine\math\Vector3;
+use wortie\PlotVote\PlotVote;
+
+class LBTask extends Task {
+	
+	private $database;
+	
+	public function onRun(int $currentTick) {
+		$level = Server::getInstance()->getDefaultLevel();
+		foreach ($level->getEntities() as $entity) {
+			if ($entity instanceof LbEntity) {
+				$entity->setNameTagAlwaysVisible(true);
+				$entity->setImmobile(true);
+				$entity->setScale(1);
+				$pv = Server::getInstance()->getPluginManager()->getPlugin("PlotVote");
+				$entity->setNameTag($pv->getDatabase()->getTop());
+			}
+		}
+	}
+
+	
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # PlotVote
- A PMMP Plugin Made for plot servers
+ A PMMP Plugin Made for plot servers, using Sqlite. Tested on 1.14.3
+ TODO: (Currently works to a certain extent.)
+  - Need help handling players who have more than one plot.
+  - Maybe Add a session handler
+  - If you find errors dont hesitate to help!
+ Features:
+  - SqLite Support
+  - LeaderBoard
+ Commands: 
+  - /plotvote
+  - /pvlb
+  - /remlb
+  - /pvtop
