26b92991d56f0e8a236a3df3cd37c74bacf8ddcc
Atech <mc8atech@gmail.com>
2020-03-23 13:39:40 +0000

    Larger Update
    Update contains a fully working plugin :P
    Updates to fix crashes related to players owning multiple plots, fixed aswell as a version  bump.

---
+++ PlotVote/Listeners/LbListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace wortie\PlotVote\listeners;
+
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\entity\EntityDamageEvent;
+
+use wortie\PlotVote\PlotVote;
+use wortie\PlotVote\Entity\LbEntity;
+
+class LbListener implements Listener {
+
+    public $plugin;
+
+    public function __construct(PlotVote $plugin) {
+        $this->plugin = $plugin;
+    }
+
+	public function onHitLB(EntityDamageByEntityEvent $event) {
+		$lb = $event->getEntity();
+		$player = $event->getDamager();
+		if ($lb instanceof LbEntity && $player instanceof Player) {
+			$event->setCancelled(true);
+			$player->sendPopup(TextFormat::GOLD . ">".TextFormat::GRAY." This is the plot LeaderBoard ".TextFormat::GOLD."<");
+		}
+	}
+}
